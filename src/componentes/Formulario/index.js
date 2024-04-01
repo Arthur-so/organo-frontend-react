fb5e086cecf22412e7d1b03cdcdd7df07d15ca98
Arthur-so <arthursantos.oliveira001@gmail.com>
2024-04-01 14:19:59 -0300

    Lista suspensa

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,7 +1,19 @@
 import CampoTexto from '../CampoTexto'
 import './Formulario.css'
+import ListaSuspensa from '../ListaSuspensa'
 
 const Formulario = () => {
+
+    const times = [
+        'Programação',
+        'Front-end',
+        'Data Science',
+        'UX e Design',
+        'Devops',
+        'Mobile',
+        'Inovação e Gestão'
+    ]
+    
     return(
     <section className='formulario'>
         <form>
@@ -9,6 +21,7 @@ const Formulario = () => {
             <CampoTexto label="Nome" placeholder="Digite seu nome"/>
             <CampoTexto label="Cargo" placeholder="Digite seu cargo"/>
             <CampoTexto label="Imagem"placeholder="Digite o endereço da imagem"/>
+            <ListaSuspensa label="Time" itens={times} />
         </form>
     </section>)
 
