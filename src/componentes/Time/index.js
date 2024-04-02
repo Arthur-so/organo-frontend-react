db72cd9c2b449a1f9e4dc7f828a5053ad90acae3
Arthur-so <arthursantos.oliveira001@gmail.com>
2024-04-02 13:17:49 -0300

    fix erros e cor cabeçalho

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -2,14 +2,14 @@ import './Time.css'
 import Colaborador from '../Colaborador'
 
 const Time = (props) => {
-    const css = {backgroundColor: props.corSecundaria}
+    const cssCorSecundaria = {backgroundColor: props.corSecundaria}
     
     return (
         props.colaboradores.length > 0 &&
-        <section className='time' style={css}>
+        <section className='time' style={cssCorSecundaria}>
             <h3 style={{borderColor: props.corPrimaria}}>{props.nome}</h3>
             <div className='colaboradores'>
-                {props.colaboradores.map(colaborador => <Colaborador nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem}/>)}
+                {props.colaboradores.map(colaborador => <Colaborador key={colaborador.nome} corFundo={props.corPrimaria} nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem}/>)}
             </div>
         </section>
     )
